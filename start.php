e0781b02683ca10c7750394d08b376b73b2e6955
Ersin Aktekin <aktekie1@eng064.cs.man.ac.uk>
2012-02-07 14:48:15 +0000

    html files

---
+++ start.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>RSS Nibbles</title>
+<link rel="shortcut icon" href="images/favicon0.ico">
+<link rel="stylesheet" type="text/css" media="all" href="images/style.css">
+</head>
+
+<body>
+
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php 
+require_once("start.php");
+echo "asdasd";
+require_once("finish.php");
+?>
